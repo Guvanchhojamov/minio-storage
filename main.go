8a4497fcc6ac1b68e5070a53fe7ec04bdc6b5dc4
Guvanchhojamov <name@mail.com>
2023-12-19 15:04:41 +0500

    some changes

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/minio/minio-go/v7"
+	"log"
+)
 
 func main() {
-	fmt.Println("Test sotrage minio")
+	endpoint := "127.0.0.1:9002"
+	useSSl := false
+	minioClient, err := minio.New(endpoint, &minio.Options{Secure: useSSl})
+	if err != nil {
+		log.Fatal(err)
+	}
+	buckOpts := minio.MakeBucketOptions{ObjectLocking: false}
+	err = minioClient.MakeBucket(context.Background(), "first", buckOpts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Test sotrage minio: %v\n %b", minioClient)
 }
